2c3790b2254a8b4811dab1479be0da0e0a81dd3a
Miks Zvirbulis <mikszvirbulis@yahoo.com>
2019-04-10 11:21:27 +0100

    Protected routes from logged out users

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '../store'
 
 // Routes
 import NotFound from '../views/404'
@@ -11,15 +12,25 @@ import Logout from '../views/Logout'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: Home },
-    { path: '/add', component: Add },
+    { path: '/add', component: Add, meta: { requiredLogin: true } },
     { path: '/signup', component: Signup },
     { path: '/login', component: Login },
-    { path: '/logout', component: Logout },
+    { path: '/logout', component: Logout, meta: { requiredLogin: true } },
     { path: '/404', component: NotFound },
     { path: '*', redirect: '/404' },  
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiredLogin) && (store.state.auth.loggedIn === false)) {
+    next("/login")
+  } else {
+    next()
+  }
+})
+
+export default router
